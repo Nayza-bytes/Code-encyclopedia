b53b4662386f58867b061a01a8ad5865cd3ec5f9
Nayza-bytes <85430899+Nayza-bytes@users.noreply.github.com>
2022-10-02 17:45:04 +0200

    Added files for the website app and added simple flask test

--- URL Shortener project/URLShortener.py
+++ URL Shortener project/URLShortener.py
@@ -1,2 +1,22 @@
 #Created by Nayza-bytes
 #Pushed on github: 02/10/2022
+
+from flask import Flask
+
+#Create the flask app
+app = Flask(__name__)
+
+#Test if our flask app is working with this function
+@app.route('/')
+def hello():
+    return "Hello world!"
+
+#The '@app.route' will be useful so when we are typing the url we can enter nayza at the end
+#And have a different message
+@app.route('/nayza')
+def hi():
+    return "Hi welcom coder"
+
+#Run the flask app on port 500 with debug
+if __name__ == '__main__':
+    app.run(port = 500, debug = True)
